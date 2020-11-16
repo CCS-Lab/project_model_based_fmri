576300573bf16ba4d47f7f3c1764825061ca0c65
Cheoljun Cho <cjfwndnsl@snu.ac.kr>
2020-11-16 13:46:40 +0900

    modify regressor.py

--- model_based_mvpa/models/regressor.py
+++ model_based_mvpa/models/regressor.py
@@ -30,162 +30,21 @@ import logging
 
 
 logging.basicConfig(level=logging.INFO)
-
-
-def get_map(train_generator,
-            val_generator,
-            model,
-            model2actmap_fun,
-            val_threshold):
-    
-    train_generator.on_epoch_end()
-    for i in range(len(train_generator)):
-        X,y = train_generator.__getitem__(i)
-        model.fit(X,y,verbose=0)
+ 
         
-    preds = []
-    answers = []
-    for i in range(len(val_generator)):
-        X,y = val_generator.__getitem__(i)
-        preds.append(model.predict(X))
-        answers.append(y)
-        
-    preds = np.concatenate(preds,0).flatten()
-    answers = np.concatenate(answers,0).flatten()
-    
-    mse = mean_squared_error(preds, answers)
-    if mse < val_threshold :
-        actmap = model2actmap_fun(model)
-    else:
-        actmap = None
-        
-    return mse, actmap
-
-
-def mlp_regression_v2(X, y,
-                   save_path=None,
+def mlp_regression(X, y, # input data
+                   # specification for model design & training
+                   temp_path=None,
                    layer_dims=[1024,1024],
                    activation_func='linear',
                    dropout_rate=0.5,
                    epochs=100,
                    patience=10,
                    batch_size=64,
-                   N=15,
-                   verbose=0):
-    
-    if verbose > 0:
-        logging.info('start running')
-        
-    coefs = []
-
-    for i in range(N):
-        
-        ids = np.arange(X.shape[0])
-        
-        train_ids, test_ids = train_test_split(
-            ids, test_size=0.2, random_state=42 + (i * i)
-        )
-        
-        train_steps = len(train_ids) // batch_size
-        val_steps = len(test_ids) // batch_size
-
-        X_train = X[train_ids]
-        X_test = X[test_ids]
-        y_train = y[train_ids]
-        y_test = y[test_ids]
-
-        train_generator = loader.DataGenerator(X_train, y_train, batch_size, shuffle=True)
-        val_generator = loader.DataGenerator(X_test, y_test, batch_size, shuffle=False)
-        
-        
-        model = Sequential()
-        model.add(Dense(layer_dims[0],
-                        activation=activation_func, input_shape=(X.shape[-1],),
-                        use_bias=False)
-        )
-        model.add(Dropout(dropout_rate))
-
-        for dim in layer_dims[1:]:
-            model.add(Dense(dim, activation=activation_func, use_bias=False))
-            model.add(Dropout(dropout_rate))
-
-        model.add(Dense(1, activation='linear',use_bias=True))
-        model.compile(loss='mse', optimizer='adam')
-        
-        def model2actmap_fun(model):
-            weights = []
-            for layer in model.layers:
-                if 'dense' not in layer.name:
-                    continue
-                weights.append(layer.get_weights()[0])
-
-            coef = weights[0]
-            for weight in weights[1:]:
-                coef = np.matmul(coef,weight)
-                
-            return coef.ravel()
-        
-        best_mse = 10000
-        patience_cnt = 0
-        #bin_rolling_i = 0
-        #bin_dict = {}
-        mse_to_actmap = {(best_mse+i):None for i in range(patience)}
-        
-        for e in range(epochs):
-            if patience_cnt > patience:
-                break
-                
-            val_threshold = max(mse_to_actmap.keys())
-            mse,act_map = _get_map(train_generator,
-                                val_generator,
-                                model,
-                                model2actmap_fun,
-                                val_threshold)
-            
-            if act_map is not None:
-                mse_to_actmap[mse] = act_map
-                del mse_to_actmap[val_threshold]
-
-            #bin_dict[bin_rolling_i] = act_map
-            #bin_rolling_i += 1
-            #bin_rolling_i = bin_rolling_i % patience
-            
-            if mse < best_mse:
-                best_mse = mse
-                patience_cnt = 0
-            else:
-                patience_cnt += 1
-            
-        errors = []
-        for mse, am in mse_to_actmap.items():
-            coefs.append(am)
-            errors.append(mse) 
-            
-        error = np.array(errors).mean()
-        
-        #for _, am in bin_dict.items():
-            #coefs.append(am)
-            
-        if verbose > 0:
-            logging.info(f'[{i+1}/{N}] - mse: {error:.04f}')
-
-    coefs = np.array(coefs)
-    
-    # coefs : N x voxel #
-    return coefs
-    
-        
-def mlp_regression(X, y,
-                   save_path=None,
-                   layer_dims=[1024,1024],
-                   activation_func='linear',
-                   dropout_rate=0.5,
-                   epochs=100,
-                   patience=10,
-                   batch_size=64,
+                   val_ratio=0.2,
                    N=15,
                    verbose=0,
-                   n_jobs=0):
+                   max_use_sample_N=30000):
     
     if verbose > 0:
         logging.info('start running')
@@ -194,11 +53,11 @@ def mlp_regression(X, y,
 
     for i in range(N):
         ids = np.arange(X.shape[0])
-        #data_N = min(max_use_sample_N,X.shape[0])
-        #np.random.shuffle(ids)
-        #ids = ids[:data_N]
+        if X.shape[0] > max_use_sample_N:
+            np.random.shuffle(ids)
+            ids = ids[:max_use_sample_N]
         train_ids, test_ids = train_test_split(
-            ids, test_size=0.2, random_state=42 + (i * i)
+            ids, test_size=val_ratio, random_state=42 + (i * i)
         )
         train_steps = len(train_ids) // batch_size
         val_steps = len(test_ids) // batch_size
@@ -211,7 +70,10 @@ def mlp_regression(X, y,
         train_generator = loader.DataGenerator(X_train, y_train, batch_size, shuffle=True)
         val_generator = loader.DataGenerator(X_test, y_test, batch_size, shuffle=False)
         
-        bst_model_path = f'temp_{i}_best_mlp.h5'
+        if temp_path is None:
+            temp_path = os.getcwd()
+        bst_model_path = os.path.join(temp_path, f'temp_id{random.random():.03f}_best_mlp.h5')
+        
         mc = ModelCheckpoint(
             bst_model_path,
             save_best_only=True, save_weights_only=True,
@@ -270,13 +132,16 @@ def mlp_regression(X, y,
     
 
 def penalized_linear_regression(X, y,
+                                temp_path=None,
                                 alpha=0.001, # mixing parameter
                                 lambda_par=0.8, # shrinkage parameter
                                 epochs=100,
                                 patience=30,
-                                batch_size=256,
+                                batch_size=64,
+                                val_ratio=0.2,
                                 N=15,
-                                verbose=0):
+                                verbose=0,
+                                max_use_sample_N=30000):
     
     if verbose > 0:
         logging.info('start running')
@@ -285,12 +150,12 @@ def penalized_linear_regression(X, y,
 
     for i in range(N):
         ids = np.arange(X.shape[0])
-        #data_N = min(max_use_sample_N,X.shape[0])
-        #np.random.shuffle(ids)
-        #ids = ids[:data_N]
+        if X.shape[0] > max_use_sample_N:
+            np.random.shuffle(ids)
+            ids = ids[:max_use_sample_N]
         
         train_ids, test_ids = train_test_split(
-            ids, test_size=0.2, random_state=42 + (i * i))
+            ids, test_size=val_ratio, random_state=42 + (i * i))
         train_steps = len(train_ids) // batch_size
         val_steps = len(test_ids) // batch_size
 
@@ -302,7 +167,10 @@ def penalized_linear_regression(X, y,
         train_generator = loader.DataGenerator(X_train, y_train, batch_size, shuffle=True)
         val_generator = loader.DataGenerator(X_test, y_test, batch_size, shuffle=False)
 
-        bst_model_path = f'temp_{i}_best_mlp.h5'
+        if temp_path is None:
+            temp_path = os.getcwd()
+        bst_model_path = os.path.join(temp_path, f'temp_id{random.random():.03f}_best_pnl.h5')
+        
         es = EarlyStopping(monitor='val_loss', patience=patience)
         mc = ModelCheckpoint(bst_model_path,
                              save_best_only=True,
@@ -348,7 +216,8 @@ def elasticnet(X, y,
                alpha=0.001,
                n_jobs=16,
                N=3,
-               verbose=0):
+               verbose=0,
+               max_use_sample_N=30000):
     
     if verbose > 0:
         logging.info('start running')
@@ -357,9 +226,9 @@ def elasticnet(X, y,
 
     for i in range(N):
         ids = np.arange(X.shape[0])
-        #data_N = min(max_use_sample_N,X.shape[0])
-        #np.random.shuffle(ids)
-        #ids = ids[:data_N]
+        if X.shape[0] > max_use_sample_N:
+            np.random.shuffle(ids)
+            ids = ids[:max_use_sample_N]
         X_data = X #[ids]
         y_data  = y #[ids]
         model = ElasticNet(alpha = alpha, n_jobs =n_jobs)
