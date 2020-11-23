af3d031fdebc0929742608c50a14cbd59b103b75
Cheoljun Cho <cjfwndnsl@snu.ac.kr>
2020-11-23 13:37:04 +0900

    add_comments

--- model_based_mvpa/utils/coef2map.py
+++ model_based_mvpa/utils/coef2map.py
@@ -18,12 +18,38 @@ from pathlib import Path
 def get_map(coefs, masked_data, layout=None, task_name=None,
             map_type='z', save_path=None, smoothing_sigma=1):
 
+    """
+    make nii image file from coefficients of model using masking info.
+    
+    ## parameters ##
+    
+    @coefs : extracted coefs by fitting model. shape : N x voxel #
+    @masked_data  : a binary nii image of masking info. type : nibabel.nifti1.Nifti1Image, 
+                    should be same shape with the original fMRI image data
+    @layout : layout info to get task_name if None, task_name should not be None instead
+    @task_name : if provided, the saved nii file will be named after this
+    @map_type : the type of making map. 'z' or 't'
+                'z' : z_map by mean z_score among N mapped images.
+                't' : t_map by voxel-wise one sample t-test among N mapped images.
+    @save_path : path to save file. if None, then will be saved in 'results' directory fo current working directory
+                the result nii image file will be saved in 'save_path/{task_name}_{map_type}_map.nii'
+    @smoothing_sigma : the sigma value for spatial gaussian smoothing for each converted map.  
+                       if 0, there will be no spatial smoothing of extracted coefficients.
+                       higher the value, more smoother the final map will be
+                       
+    ## return ##
+    
+    @result : a nii image of brain activation map.
+    
+    """
     assert (not(layout is None and task_name is None))
     
     activation_maps = []
     mapping_id = np.nonzero(masked_data.get_fdata().flatten())[0]
     
     for coef in coefs:
+        
+        #converting flattened coefs to brain image.
         activation_map = np.zeros(masked_data.get_fdata().flatten().shape[0])
         for i, v in zip(mapping_id, coef):
             activation_map[i] = v
@@ -37,14 +63,17 @@ def get_map(coefs, masked_data, layout=None, task_name=None,
     activation_maps[np.isnan(activation_maps)] = 0
 
     if map_type == 't':
+        # voxel-wise one sample ttest
         m = ttest_1samp(activation_maps, 0).statistic
     else:
+        # averaging z_score of each voxel
         m = zscore(activation_maps, axis=None).mean(0)
         
     m[np.isnan(m)] = 0
     m *= masked_data.get_fdata()
     result = nib.Nifti1Image(m, affine=masked_data.affine)
     
+    # saving
     if save_path is None:
         sp = Path('./results')
     else: 
