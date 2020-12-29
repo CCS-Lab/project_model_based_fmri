2b26856f488032ba0be1cb05cadcfdbe78469894
Cheoljun Cho <cjfwndnsl@snu.ac.kr>
2020-12-29 13:32:16 +0900

    add test

--- tests/test-base.py
+++ tests/test-base.py
@@ -6,3 +6,5 @@ from mbmvpa.utils.example_utils import load_example_data
 from mbmvpa.utils.coef2map import get_map
 from time import perf_counter
 from mbmvpa.models.regressor import penalized_linear_regression, mlp_regression, elasticnet
+
+import pytest
