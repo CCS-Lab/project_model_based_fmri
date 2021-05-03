aabc6933dcf38a65886f71219321aa3ad2302242
Cheoljun Cho <cjfwndnsl@snu.ac.kr>
2021-05-03 13:40:16 +0900

    update

--- tutorials/test_oneshot.py
+++
@@ -1,8 +0,0 @@
-from mbmvpa.core.engine import MBMVPA
-
-model = MBMVPA(root='tutorial_data/ccsl_prl',
-           dm_model='prl_fictitious_rp_woa',
-           task='prl',
-           process='PEchosen')
-
-_ = model.run()
