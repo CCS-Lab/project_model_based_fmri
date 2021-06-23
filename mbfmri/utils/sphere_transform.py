b06787c8607820dfd4b1f8f5474d58ba04b3a5ac
Cheoljun Cho <cjfwndnsl@snu.ac.kr>
2021-06-23 16:10:30 +0900

    update

---
+++ mbfmri/utils/sphere_transform.py
@@ -0,0 +1,78 @@
+import numpy as np
+import nibabel as nib
+from nilearn import surface
+from nilearn import datasets
+import cv2
+
+class ToFlattenedSphere():
+    def __init__(self,shape=(180,180)):
+        self.fsaverage = datasets.fetch_surf_fsaverage(mesh='fsaverage')
+        self.shape = shape
+        self._set_mapping()
+        
+    def _set_mapping(self):
+        self.map_right = self._spherical2map(self._cartesian2spherical(surface.load_surf_mesh(self.fsaverage.infl_right)[0]),shape=self.shape)
+        self.map_left = self._spherical2map(self._cartesian2spherical(surface.load_surf_mesh(self.fsaverage.infl_left)[0]),shape=self.shape)
+        
+    def _cartesian2spherical(self, xyz):
+        #takes list xyz (single coord)
+        x       = xyz[:,0]
+        y       = xyz[:,1]
+        z       = xyz[:,2]
+        r       =  np.sqrt(x*x + y*y + z*z)
+        elevation   =  np.arccos(z/r)  #to degrees
+        azimuth     =  np.arctan2(y,x) 
+        return np.array([r,elevation,azimuth]).transpose(1,0)
+    
+    def _spherical2map(self,spherical_coords,shape=(180,180)):
+        rea = spherical_coords.copy()
+        rea[:,1] /= np.pi
+        rea[:,2] /= (2*np.pi)
+        rea[:,2] += .5
+        rea[:,1] *= shape[0]
+        rea[:,2] *= shape[1]
+        mapping = []
+        for _,e,a in rea:
+            e,a = int(e),int(a)
+            mapping.append([e,a])
+
+        mapping = np.array(mapping)
+        return mapping
+    
+    def _make2dmap(self,texture,mapping,shape):
+        bg = np.zeros(shape)
+        bg_cnt = np.zeros(shape)
+        for (e,a),v in zip(mapping,texture):
+            bg[e,a] += v
+            bg_cnt[e,a] += 1
+        bg_cnt[bg_cnt==0] = 1
+        flatmap = bg/bg_cnt
+        return flatmap
+    
+    def _realign_flatmap(self,flatmap,mapping):
+        
+        return np.array([flatmap[e,a] for e,a in mapping])
+        
+    def flatten(self,img):
+        
+        texture_right = surface.vol_to_surf(img, self.fsaverage.infl_right,inner_mesh=self.fsaverage.white_right)
+        texture_right[np.isnan(texture_right)]=0
+        
+        flatmap_right = self._make2dmap(texture_right,self.map_right,shape=self.shape)
+        texture_left = surface.vol_to_surf(img, self.fsaverage.infl_left,inner_mesh=self.fsaverage.white_left)
+        texture_left[np.isnan(texture_left)]=0
+        flatmap_left = self._make2dmap(texture_left,self.map_left,shape=self.shape)
+        flatmaps = np.array([flatmap_right,flatmap_left]).transpose(1,2,0)
+        #kernel = np.ones((3,3),np.float32)/9
+        #flatmaps = cv2.filter2D(flatmaps, -1, kernel)
+        return flatmaps
+    
+    def reconstruct(self,flatmaps):
+        texture_right = self._realign_flatmap(flatmaps[:,0])
+        texture_left = self._realign_flatmap(flatmaps[:,1])
+        
+        # affine??
+
+# add interpolation 
+# https://docs.scipy.org/doc/scipy/reference/tutorial/interpolate.html#multivariate-data-interpolation-griddata
+# the cnn should have a large receptive field... (with dilated one?)
