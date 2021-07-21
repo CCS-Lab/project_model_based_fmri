bd7ebb1bb1e4feed8f9793549b55b1fb3c59b6de
cheoljun <cjfwndnsl@snu.ac.kr>
2021-07-21 19:35:25 +0900

    update

--- mbfmri/preprocessing/bold.py
+++ mbfmri/preprocessing/bold.py
@@ -32,105 +32,153 @@ class VoxelFeatureGenerator():
     Parameters
     ----------
     
-    bids_layout : str or pathlib.PosixPath or bids.layout.layout.BIDSLayout
-        (Original) BIDSLayout of input data. It should follow BIDS convention.
-        The main data used from this layout is behaviroal data,``events.tsv``.
+    bids_layout : str or pathlib.PosixPath or bids.layout.layout.BIDSLayout or BIDSController
+        Root for input data. It should follow **BIDS** convention.
+
     subjects : list of str or "all",default="all"
         List of subject IDs to load. 
         If "all", all the subjects found in the layout will be loaded.
-    bids_controller : mbmvpa.utils.bids_utils.BIDSController, default=None
-        BIDSController instance for controlling BIDS layout for preprocessing.
-        If not given, then initiates the controller.
+
     save_path : str or pathlib.PosixPath, default=None
-        Path for saving preprocessed results. The MB-MVPA BIDS-like derivative layout will be created under the given path.
-        If not input by the user, it will use "BIDSLayout_ROOT/derivatives/."
+        Path for saving preprocessed results. If not given, the derivatives directory will be used.
+
+
     task_name : str, default=None
         Name of the task. If not given, the most common task name will be automatically selected.
+    
     feature_name : str, default="unnamed"
         Name for indicating preprocessed feature.
         This is adopted for distinguishing different configuration of feature processing.
+    
     fmriprep_name : str, default="fMRIPrep"
         Name for derivatve BIDS layout for fmriprep.
         As default, it is "fMRIPrep", which is the name of preprocessing by **fMRIPrep**.
         (https://fmriprep.org/en/stable/)
+
     mask_path : str or pathlib.PosixPath, default=None
         Path for directory containing mask files. 
+        If None, the default mask_path is 'BIDS_ROOT/masks.'
+        Then, images in mask_path/include will be used to create a mask to include and 
+        images in mask_path/exclude will be used to create a mask to exclude
         Mask files are nii files recommended to be downloaded from **Neurosynth**.
         (https://neurosynth.org/)
         As default, each of the nii files is regarded as a probablistic map, and
         the *mask_trheshold* will be used as the cut-off value for binarizing.
         The absolute values are used for thresholding.
         The binarized maps will be integrated by union operation to be a single binary image.
-        If None, the default mask_path is 'fmriprep_ROOT/masks.'
-    bold_suffix : str, default='bold'
-        Name of suffix indicating 'bold' image.
-        It will be used for searching image files through BIDS layout.
-    confound_suffix : str, default='regressors'
-        Name of suffix indicating confounds file
-        It will be used for searching confounds files through BIDS layout.
-    mask_threshold : float, default=2.58
+    
+    mask_threshold : float, default=1.65
         Cut-off value for thresholding mask images. 
-        The default value, 2.58 is determined as it assume the input masks as z maps,
-        and 99% confidence interval is considered.
-    zoom : tuple[float, float, float], defualt=(2,2,2)
+        The default value (=1.65) means the boundary of 
+        upper 90% in normal distribution.
+
+    mask_smoothing_fwhm : float, default=6
+        Size in millimeters of the spatial smoothing of mask images.
+        If None, smoothing is skipped.
+
+    include_default_mask : bool, default=True
+        Indicate if the default mask (mni space) should be applied.
+
+    gm_only : bool, default=False
+        Indicate if gray matter mask should be applied
+
+    atlas : str, default=None
+        Name of atlas when masking by ROIs.
+        #TODO add link for list 
+
+    rois : list of str, default=[]
+        Names or ROI when masking by ROI.
+        #TODO add link for list 
+
+    zoom : (int,int,int),  default=(2,2,2)
         Window size for zooming fMRI images. Each of three components means x, y ,z axis respectively.
         The size of voxels will be enlarged by the factor of corresponding component value.
         Ex. zoom = (2,2,2) means the original 2 mm^3 voxels will be 4mm^3, so reducing the total number of
         voxels in a single image.
+
     smoothing_fwhm : float, default=None
-        If smoothing_fwhm is not None, it gives the size in millimeters of the
-        spatial smoothing to apply to the signal.
+        Size in millimeters of the spatial smoothing of fMRI.
+        If None, smoothing is skipped.
+    
     standardize : boolean, default=True
-        If True, standardization (Gaussian normalization) would be done for each image.
-    confounds : list of str, default=[]
-        The list of names for indicating columns in confounds files.
-        The values of *confounds* will be regressed out. 
+        Indicate tf standardization is required for fMRI. 
+
     high_pass : float, default = 1/128
         Value for high pass filter. [Hz]
+    
     detrend : boolean, default=False
         If True, remove a global linear trend in data.
+
+    bold_suffix : str, default='bold'
+        Name of suffix indicating 'bold' image.
+        It will be used for searching image files through BIDS layout.
+    
+    confounds : list of str, default=[]
+        Names of confound factors to be regressed out.
+        Each should be in the columns of confound files.
+
     n_thread : int, default=4
         Number of threads for multi-processing. 
         Please consider your computing capcity and enter the affordable number.
+    
     ignore_original : boolean, default=True
-        Indicator to tell whether it would cover behaviroal data in the original BIDSLayout.
+        Indicate whether it would cover behaviroal data in the original BIDSLayout.
         If True, it will only consider data in the derivative layout for fMRI preprocessed data.
     
+    space_name : str, default=None
+        Name of template space. If not given, the most common space in 
+        input layout will be selected. 
+    
+    t_r : float, default=None
+        Time resolution in second. 
+        It will be overrided by value from input data if applicable.
+    
+    slice_time_ref: float, default=.5
+        Slice time reference in ratio in 0,1].
+        It will be overrided by value from input data if applicable.
+
+    bold_suffix : str, default='regressors'
+        Name of suffix indicating preprocessed fMRI file
+        
+    confound_suffix : str, default='regressors'
+        Name of suffix indicating confounds file
+
     """
     
     def  __init__(self,
                   bids_layout,
                   subjects="all",
                   sessions="all",
-                  bids_controller=None,
                   save_path=None,
                   task_name=None,
                   space_name=None,
                   feature_name="unnamed",
                   fmriprep_name='fMRIPrep',
                   mask_path=None,
-                  bold_suffix='bold',
-                  confound_suffix='regressors',
-                  mask_threshold=1.65, # for 99.9 two-tailed confidence : 3.29, # for 99 % two-tailed confidence : 2.58,
-                  zoom=(2, 2, 2),
-                  smoothing_fwhm=6,
+                  mask_threshold=1.65,
                   mask_smoothing_fwhm=6,
                   include_default_mask=True,
+                  gm_only=False,
                   atlas=None,
                   rois=[],
-                  gm_only=False,
+                  zoom=(2, 2, 2),
+                  smoothing_fwhm=6,
                   standardize=True,
-                  confounds=[],
                   high_pass=1/128,
                   detrend=True,
+                  confounds=[],
                   n_thread=4,
                   ignore_original=True,
                   t_r=None,
                   slice_time_ref=.5,
+                  bold_suffix='bold',
+                  confound_suffix='regressors',
                   **kwargs):
         
         # set path informations and load layout
-        if bids_controller is None:
+        if isinstance(bids_layout,BIDSController):
+            self.bids_controller = bids_controller
+        else:
             self.bids_controller = BIDSController(bids_layout,
                                             subjects=subjects,
                                             sessions=sessions,
@@ -143,8 +191,6 @@ class VoxelFeatureGenerator():
                                             ignore_original=ignore_original,
                                             t_r=t_r,
                                             slice_time_ref=slice_time_ref,)
-        else:
-            self.bids_controller = bids_controller
             
         self.bids_controller._set_voxelmask_path(feature_name=feature_name)
         
@@ -206,6 +252,10 @@ class VoxelFeatureGenerator():
             confounds=None,
             n_thread=None,
             **kwargs):
+
+        """Generate multi-voxel feature data (masking & zooming & cleaning).
+    
+        """
         
         if n_thread is None:
             n_thread = self.n_thread
