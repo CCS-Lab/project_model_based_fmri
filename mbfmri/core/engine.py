4f5a4e3ccddbc046e4981ed5885c367b7e1f17cb
Cheoljun Cho <cjfwndnsl@snu.ac.kr>
2021-08-05 00:46:00 +0900

    update

--- mbfmri/core/engine.py
+++ mbfmri/core/engine.py
@@ -16,9 +16,25 @@ valid_analysis = ['mvpa','glm']
 
 def run_mbfmri(analysis='mvpa',
               **kwargs):
-    if analysis.lower() == 'mvpa':
+    
+    """Top wrapper function for model-based fMRI analysis.
+    It offers GLM approach and MVPA approach.
+    
+    Parameters
+    ----------
+    
+    analysis: str, default="mvpa"
+        Name of approach. "mvpa" will conduct MVPA approach by running *run_mbmvpa*,
+        and "glm" will conduct GLM approach by running *run_mbglm*.
+    
+    kwargs: dict
+        Kewarded dictionary for configuration. 
+        Please check the documentation of submodules.
+        
+    """
+    if analysis.lower() == "mvpa":
         run_mbmvpa(**kwargs)
-    elif analysis.lower() == 'glm':
+    elif analysis.lower() == "glm":
         run_mbglm(**kwargs)
     else:
         raise ValueError(f'ERROR: please enter valid analysis type-{valid_analysis}')
